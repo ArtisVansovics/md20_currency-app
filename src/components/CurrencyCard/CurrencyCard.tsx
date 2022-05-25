a5a39902ba0701814368950cea4478097dadc44e
Artis Vansovics <vansovics.artis@gmail.com>
2022-05-26 00:59:50 +0300

    Created CurrencyCard component

---
+++ src/components/CurrencyCard/CurrencyCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './CurrencyCard.module.scss';
+import Button from '../Button/Button';
+
+type CurrencyCardProps = {
+  code: string;
+  name: string;
+}
+
+const CurrencyCard = ({ code, name }: CurrencyCardProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.card}>
+      <h2 className={styles.title}>
+        {name}
+      </h2>
+      <h3 className={styles.code}>
+        {`Code: ${code}`}
+      </h3>
+      <Button
+        title="See Rates"
+        onClick={() => navigate(`/currencies/${code}`)}
+      />
+    </div>
+  );
+};
+export default CurrencyCard;
