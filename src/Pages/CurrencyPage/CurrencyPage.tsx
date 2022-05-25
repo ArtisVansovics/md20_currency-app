a50531ac7f7432d7bd528e09354c3ccee78870fd
Artis Vansovics <vansovics.artis@gmail.com>
2022-05-26 02:12:03 +0300

    Currency page now shows exchange rates for chosen currency

--- src/Pages/CurrencyPage/CurrencyPage.tsx
+++ src/Pages/CurrencyPage/CurrencyPage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetCurrencyByCodeQuery } from '../../store/api/currencyApi';
+import Loader from '../../components/Loader/Loader';
+import CurrencyBox from '../../components/CurrencyBox/CurrencyBox';
 
 const CurrencyPage = () => {
-  const a = 0;
+  const { code } = useParams();
+  const { data, isLoading } = useGetCurrencyByCodeQuery(String(code));
 
   return (
     <div className="page">
@@ -9,7 +14,17 @@ const CurrencyPage = () => {
         <div className="row">
           <div className="col-xs-12">
             <div className="box">
-              <h1 className="page__title">Currency Page</h1>
+              <h1 className="page__title">{code && code.toUpperCase()}</h1>
+              {isLoading && <Loader />}
+              <div className="grid-auto grid-auto--narrow">
+                {data && Object.entries(data[String(code)]).map((currency) => (
+                  <CurrencyBox
+                    key={currency[0]}
+                    code={currency[0]}
+                    value={Number(currency[1])}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
